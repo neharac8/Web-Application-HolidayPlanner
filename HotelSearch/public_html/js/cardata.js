168ac08ff6c1a10526bb271b37aa206a5bed81d2
hpadmin <hpadmin>
2015-10-27 15:24:57 -0700

    Added HolidayPlanner

---
+++ HotelSearch/public_html/js/cardata.js
@@ -0,0 +1,39 @@
+// JavaScript Document
+var cardata = '[\
+{"name" : "<a href=http://www.avis.com style=text-decoration:none >Avis</a>",\
+"styledrating":"★★★☆☆ 3.1/5",\
+"rating" : "#12 of 236 Hotels in San Francisco",\
+"description" : "4 people  3 bags ",\
+"reviews" : "Trip Advisor Reviews : 252",\
+"view":"Currently 52 people are viewing this hotel",\
+"logo":"./img/compact.jpg",\
+"partners":[{"link":"","price":"140$"},{"link":"","price":"142$"}]\
+},\
+{"name" : "<a href=http://www.budget.com/budgetWeb/home/home.ex style=text-decoration:none>Budget</a>",\
+"styledrating":"★★★☆☆ 3.4/5",\
+"rating" : "#46 of 236 Hotels in San Francisco",\
+"description" : "7 people  2 bags ",\
+"reviews" : "Trip Advisor Reviews : 252",\
+"view":"Currently 52 people are viewing this hotel",\
+"logo":"./img/Minivan.jpg",\
+"partners":[{"link":"","price":"140$"},{"link":"", "price":"142$"}]\
+},\
+{"name" : "<a href=http://www.dollar.com style=text-decoration:none >Dollar</a>",\
+"styledrating":"★★★☆☆ 4.1/5",\
+"rating" : "#32 of 236 Hotels in San Francisco",\
+"description" : "4 people 2 bags",\
+"reviews" : "Trip Advisor Reviews : 1352",\
+"view":"Currently 45 people are viewing this hotel",\
+"logo":"./img/luxury.jpg",\
+"partners":[{"link":"","price":"140$"},{"link":"", "price":"142$"}]\
+},\
+{"name" : "<a href=http://www.enterprise.com/car_rental/home.do style=text-decoration:none>Enterprise</a>",\
+"styledrating":"★★★☆☆ 4.1/5",\
+"rating" : "#2 of 236 Hotels in San Francisco",\
+"description" : "5 people 3 bags ",\
+"reviews" : "Trip Advisor Reviews : 252",\
+"view":"Currently 52 people are viewing this hotel",\
+"logo":"./img/fullsize.jpg",\
+"partners":[{"link":"","price":"140$"},{"link":"", "price":"142$"}]\
+}\
+]'
